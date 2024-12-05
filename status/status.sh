36e13db40c9b08c5c73e988b1ff732d1d0bd5a1e
Ryan Lieu <github-benzBrake@woai.ru>
2024-12-05 22:46:28 +0800

    feat: 支持从 cgroup 读取内存参数

--- status/status.sh
+++ status/status.sh
@@ -1,43 +1,49 @@
-#!/bin/ash
-
-output_path=""
-script_dir=$(dirname "$(readlink -f "$0")")
-
-while getopts "o:" opt; do
-    case $opt in
-        o)
-            output_path=$OPTARG
-            ;;
-        *)
-            ;;
-    esac
-done
-
-if [ -z "$output_path" ]; then
-    output_path="$script_dir/system_usage.json"
-fi
-
+#!/bin/bash
 while true; do
-    # 获取内存总量和占用量
-    mem_total=$(free -m | awk '/Mem/ {printf "%.2f", $2}')
-    mem_used=$(free -m | awk '/Mem/ {printf "%.2f", $3}')
-    mem_usage=$(free -m | awk '/Mem/ {usage = $3 / $2 * 100; printf "%.2f", usage}')
-
     # 获取CPU占用百分比
-    cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
+    cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+
+    if [ -f /sys/fs/cgroup/memory.current ]; then
+      # 获取内存总量和已用内存，并转换为MB
+      mem_total=$(cat /sys/fs/cgroup/memory.max)
+      mem_used=$(cat /sys/fs/cgroup/memory.current)
+      mem_total=$(echo "scale=2; $mem_total / (1024 * 1024)" | bc)
+      mem_used=$(echo "scale=2; $mem_used / (1024 * 1024)" | bc)
+
+      # 获取swap总量和已用swap，并转换为MB
+      swap_total=$(cat /sys/fs/cgroup/memory.swap.max)
+      swap_used=$(cat /sys/fs/cgroup/memory.swap.current)
+      swap_total=$(echo "scale=2; $swap_total / (1024 * 1024)" | bc)
+      swap_used=$(echo "scale=2; $swap_used / (1024 * 1024)" | bc)
+    else
+      # 获取内存总量和占用量
+      mem_total=$(free -m | awk '/Mem/ {printf "%.2f", $2}')
+      mem_used=$(free -m | awk '/Mem/ {printf "%.2f", $3}')
+      mem_usage=$(free -m | awk '/Mem/ {usage = $3 / $2 * 100; printf "%.2f", usage}')
+
+      # 获取swap总量和已用swap
+      swap_total=$(free -m | awk '/Swap/ {printf "%.2f", $2}')
+      swap_used=$(free -m | awk '/Swap/ {printf "%.2f", $3}')
+      swap_usage=$(free -m | awk '/Swap/ {usage = $3 / $2 * 100; printf "%.2f", usage}')
+    fi
+
+    # 计算内存占用百分比
+    mem_usage=$(echo "scale=2; $mem_used / $mem_total * 100" | bc)
+    mem_usage="${mem_usage}%"
+
+    # 计算swap占用百分比
+    swap_usage=$(echo "scale=2; $swap_used / $swap_total * 100" | bc)
+    swap_usage="${swap_usage}%"
 
     # 获取根目录占用百分比
-    disk_usage=$(df -h / | awk '/\// {usage = $(NF-1); print usage}')
-
-    # 获取硬盘总容量和已占用空间
     disk_total=$(df -h / | awk '/\// {print $2}')
     disk_used=$(df -h / | awk '/\// {print $3}')
 
     # 构建JSON对象
-    json=$(printf '{"cpu_usage": "%s%%", "mem_total": "%sMB", "mem_used": "%sMB", "mem_usage": "%s%%", "disk_usage": "%s", "disk_total": "%s", "disk_used": "%s"}' "$cpu_usage" "$mem_total" "$mem_used" "$mem_usage" "$disk_usage" "$disk_total" "$disk_used")
+    json='{ "cpu_usage": "'$cpu_usage'", "mem_usage": "'$mem_usage'", "mem_total": "'$mem_total' MB", "mem_used": "'$mem_used' MB", "swap_usage": "'$swap_usage'", "swap_total": "'$swap_total' MB", "swap_used": "'$swap_used' MB", "disk_usage": "'$disk_usage'" }'
 
     # 将JSON对象写入文件
-    printf "%s" "$json" > "$output_path"
+    echo "$json" > /srv/usr/themes/blackleft/status/system_usage.json
 
     sleep 1
 done
